38c0c357948349a0989c3e0ca4aa4d10ad566969
NwayNandar <nwaynandar2001@gmail.com>
2024-05-18 10:19:09 +0630

    first add

---
+++ src/main/java/com/example/StudentRegistrationBoot/StudentRegistrationBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentregistrationboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRegistrationBootApplication.class, args);
+    }
+
+}
